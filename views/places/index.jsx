fa42261410b6f897af08ad783b91853d9d5f532a
AliPort <bwithtacos@gmail.com>
2022-07-27 17:52:32 -0500

    added places

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,9 +4,15 @@ const Def = require('../default')
 function index (data){
     let placesFormatted = data.places.map((place) => {
         return (
-            <div>
+            <div className="col-sm-6"> 
                 <h2>{place.name}</h2>
+                <p className="text-center">
+                    {places.cuisines}
+                </p>
                 <img src={place.pic} alt={place.name}></img>
+                <p className="text-center">
+                    Located in {place.city}, {place.state}
+                </p>
             </div>
         )
     })
@@ -15,8 +21,9 @@ function index (data){
     return(
         <Def>
             <main>
-                <h1>Places Index page</h1>
-                {placesFormatted}
+                <h1>Places to Rant or Rave About</h1>
+                <div className="row">
+                {placesFormatted}</div>
             </main>
         </Def>
     )
