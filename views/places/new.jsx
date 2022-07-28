026ad3e27233350568b7fd3389f0cc3202fecea2
AliPort <bwithtacos@gmail.com>
2022-07-27 19:53:33 -0500

    attempt to jazz up form page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -7,10 +7,27 @@ function new_form () {
             <main>
                 <h1>Add a New Place</h1>
                 <form method="POST" action="/places">
-                    <div>
+                    <div className="form-group">
                         <label htmlFor="name">Place Name</label>
-                        <input/>
+                        <input className="form-control" id="name" name="name" required/>
                     </div>
+<div className="form-group">
+    <label htmlFor="pic">Place Picture</label>
+    <input className="form-control" id="pic" name="pic"/>
+</div>
+<div className="form-group">
+    <label htmlFor="city">City</label>
+    <input className="form-control" id="pic" name="city"/>
+</div>
+<div className="form-group">
+    <label htmlFor="state">State</label>
+    <input className="form-control" id="state" name="state"/>
+</div>
+<div className="form-group">
+    <label htmlFor="cuisines">Cuisines</label>
+    <input className="form-control" id="cusines" name="cusines" required/>
+</div>
+<input className="btn btn-primary" type="submit" value="Add Place"/>
                 </form>
             </main>
         </Def>
