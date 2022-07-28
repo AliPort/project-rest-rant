790d4d45b272ee416d059e4363d0572243962a07
AliPort <bwithtacos@gmail.com>
2022-07-27 20:34:37 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,7 @@
 const { application } = require('express')
 
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 /*router.get('/', (req,res)=>{
     res.send('GET/places')
@@ -13,28 +14,26 @@ router.get('/new', (req, res) => {
 
 //places index route
 router.get('/', (req, res) =>{
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
-      
-    res.render('places/index',{places})
+        res.render('places/index',{places})
 })
 
 //post.places route
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
-})
+    //console.log(req.body)
+    if (!req.body.pic) {
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
+
 
 
 module.exports=router
